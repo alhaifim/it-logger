43cba9f416ba75de08589e336b658cc107a1b24e
alhaifim <alhaifim@outlook.com>
2019-12-15 21:53:55 -0700

    Cleaned up the project, added Materialize-css, added material icons and google font

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';   // bring JavaScript
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+useEffect(()=> {
+  //initialize materialize JS
+  M.AutoInit();
+})
+  return <div className='App'>My APP</div>;
+};
 
 export default App;
